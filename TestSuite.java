2c60d130982b1ccc81d8911ad372f741155833b0
Nana <nana.d@hotmail.de>
2019-10-02 09:03:43 +0200

    adjusted TestSuite

--- TestSuite.java
+++ TestSuite.java
@@ -10,6 +10,15 @@ public class TestSuite {
     Person owner = new Person("John Doe");
     BankAccount account = new BankAccount(owner, 1000.00);
     //test
-    assertEquals(owner,account.getOwner(),0.001);
+    assertEquals(owner.getName(),account.getOwner().getName(),0.001);
+  }
+
+  //Test Bank Account
+  @Test public static void InitTest1() {
+    //define
+    Person owner = new Person("John Doe");
+    BankAccount account = new BankAccount(owner, 1000.00);
+    //test
+    assertEquals(1000.00,account.getBalance(),0.001);
   }
 }
